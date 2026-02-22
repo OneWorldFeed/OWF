75da31f50273e3e6b9a26a4b33a1a643a7fc8d13
OneWorldFeed <oneworldfeedoffical@gmail.com>
2026-02-21 21:25:17 -0800

    Implement card registry for rendering feed items
    Added card registry to map feed item types to their respective renderers.

--- modules/cards/card-registry.js
+++ modules/cards/card-registry.js
@@ -1,1 +1,24 @@
+/**
+ * OWF | One World Feed
+ * modules/cards/card-registry.js
+ *
+ * Maps feed item types to card renderers.
+ */
 
+import { renderPostCard } from './card-post.js';
+import { renderHeadlineCard } from './card-headline.js';
+import { renderDiscoverCard } from './card-discover.js';
+
+const registry = {
+  post: renderPostCard,
+  headline: renderHeadlineCard,
+  discover: renderDiscoverCard
+};
+
+export function renderCard(item) {
+  const renderer = registry[item.type];
+  if (!renderer) {
+    return `<div class="card card-unknown">Unknown card type: ${item.type}</div>`;
+  }
+  return renderer(item);
+}
