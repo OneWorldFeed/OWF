41ffc866ca99f836e61608b916af381aad810b4f
OneWorldFeed <oneworldfeedoffical@gmail.com>
2026-02-25 18:39:37 -0800

    Add routes for various views in routes.js

---
+++ modules/router/routes.js
@@ -0,0 +1,13 @@
+export const routes = {
+  home: "/views/home.html",
+  discover: "/views/discover.html",
+  news: "/views/news.html",
+  music: "/views/music.html",
+  live: "/views/live.html",
+  podcasts: "/views/podcasts.html",
+  profile: "/views/profile.html",
+  badges: "/views/badges.html",
+  ai: "/views/ai.html",
+  dm: "/views/dm.html",
+  settings: "/views/settings.html"
+};
