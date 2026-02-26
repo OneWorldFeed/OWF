4dae1eca5d2d6ee6a495698a22f004b13512b85e
OneWorldFeed <oneworldfeedoffical@gmail.com>
2026-02-25 18:51:32 -0800

    Initialize navigation on DOM content loaded

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import { initRouter } from "./modules/router/router.js";
+import { initNavigation } from "./modules/nav/nav.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     initRouter();
+    initNavigation();
 });
